23283a9ffa002ff41395f8f4d7fab323e55bf87c
Jefferson Zamora <jeffzamorachiqui97@gmail.com>
2022-12-02 15:14:35 -0600

    Insert

--- controller/categoria.php
+++ controller/categoria.php
@@ -6,11 +6,23 @@
 
     $categoria = new Categoria();
 
+    $body = json_decode(file_get_contents("php://input"), true);
+
     switch($_GET["op"]){
     case "GetAll":
         $datos = $categoria->get_categoria();
         echo json_encode($datos);
         break;
+
+    case "GetId":
+        $datos = $categoria->get_categoria_x_id($body["cat_id"]);
+        echo json_encode($datos);
+        break;
+
+    case "Insert":
+            $datos = $categoria->insert_categoria($body["cat_nom"],$body["cat_obs"]);
+            echo json_encode($datos);
+    break;
     }
 
 
